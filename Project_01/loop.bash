c6b7a21983a4238fd388971b4d4d622390bbc5a3
Rahul kumar Nalubandhu <115523842+rahulnalubandhu@users.noreply.github.com>
2023-06-27 18:03:33 -0700

    Add files via upload

---
+++ Project_01/loop.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#number of threads:
+for t in 1 2 4 6 8 12 16
+do
+  for n in 1 10 100 1000 10000 100000 500000 1000000
+  do
+     g++   project1.cpp  -DNUMT=$t -DNUMTRIALS=$n  -o project1  -lm  -fopenmp
+    ./project1
+  done
+done
